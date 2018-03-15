b2838e748fd3bad9244496c39da16ef96302ae94
匀强的磁场 <297498075@users.noreply.github.com>
2018-03-15 17:02:39 +0800

    socket

--- CM.UAM.Service/Program.cs
+++ CM.UAM.Service/Program.cs
@@ -11,27 +11,14 @@ namespace CM.UAM.Service
 {
     class Program
     {
-        private static Dictionary<String, IService> App = new Dictionary<string, string>();
         static void Main(string[] args)
         {
-            SocketServerBase server = new SocketServerBase();
-            server.NewClientAccepted += Server_NewClientAccepted;
-            server.Start();
-
-            Console.WriteLine("enter any key to exit.");
-            Console.ReadKey();
-        }
-
-        private static void Server_NewClientAccepted(Socket client, ISocketSession session)
-        {
-            Console.WriteLine("");
-            AsyncSocketSession ass = session as AsyncSocketSession;
-
-            ass.SetReceiveHandler(arg =>
+            SocketServer.Start();
+            SocketServer.SetCallBack(arg =>
             {
                 Console.WriteLine("");
                 string data = "没有找到";
-                string received = System.Text.Encoding.UTF8.GetString(arg.Buffer, arg.Offset, arg.BytesTransferred);
+                string received = arg;
                 int moduleNameLength = received.IndexOf('/');
                 String moduleName = String.Empty;
                 if (moduleNameLength == -1)
@@ -52,7 +39,7 @@ namespace CM.UAM.Service
                     data += "模块名为" + moduleName + "的模块";
                 }
 
-                ass.Send(data);
+                return data;
             });
         }
     }
